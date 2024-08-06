e8bb28e84c188ca41aa62cc9819aeef84845e6ad
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-05 22:54:30 -0300

    fix: integration adjustments

--- src/main/java/com/fiap/hackathon_payment_management/adapters/validation/ValidateCreditCard.java
+++ src/main/java/com/fiap/hackathon_payment_management/adapters/validation/ValidateCreditCard.java
@@ -8,6 +8,7 @@ import com.fiap.hackathon_payment_management.usecase.dto.PaymentDto;
 import com.fiap.hackathon_payment_management.usecase.dto.PaymentRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,19 +22,31 @@ import java.util.List;
 @Component
 public class ValidateCreditCard implements PaymentValidation {
 
-    @Value("ms-credit-card.url")
+    @Value("${ms-credit-card.url}")
     private String urlGetCreditCard;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    @Autowired
-    private GetPaymentsByClient getPaymentsByClient;
+    private final GetPaymentsByClient getPaymentsByClient;
+
+    public ValidateCreditCard(
+            RestTemplate restTemplate,
+            GetPaymentsByClient getPaymentsByClient) {
+        this.restTemplate = restTemplate;
+        this.getPaymentsByClient = getPaymentsByClient;
+    }
 
     @Override
     public void validate(PaymentRequestDto requestDto) {
 
-        var creditCard = getCreditCardByNumber(requestDto.numero());
+        // var creditCard = getCreditCardByNumber(requestDto.numero());
+
+        String url = new StringBuilder().append(urlGetCreditCard).append("/").append(requestDto.numero()).toString();
+
+        ResponseEntity<CreditCardResponseDto> result = restTemplate.getForEntity(url,
+                CreditCardResponseDto.class);
+
+        var creditCard = result.getBody();
 
         if (!creditCard.cpf().equals(requestDto.cpf())) {
             throw new ValidationException("Cartão de crédito não pertence a esse cliente.");
@@ -71,7 +84,7 @@ public class ValidateCreditCard implements PaymentValidation {
         var totalPaymentAmount = new BigDecimal(currentPaymentValue);
         var totalLimitCredit = new BigDecimal(creditCard.limit());
 
-        for (PaymentDto payment: listPayment) {
+        for (PaymentDto payment : listPayment) {
             var value = new BigDecimal(payment.getValor());
             totalPaymentAmount = totalPaymentAmount.add(value);
         }
@@ -84,31 +97,30 @@ public class ValidateCreditCard implements PaymentValidation {
         List<CreditCardResponseDto> listMock = Arrays.asList(
                 new CreditCardResponseDto("", "69919462065", "1000", "5465 1946 1186 2985", "01/2020", "542"),
                 new CreditCardResponseDto("", "69919462063", "1000", "5465 1946 1186 2986", "01/2026", "542"),
-                new CreditCardResponseDto("", "69919462069", "1000", "5465 1946 1186 2989", "04/XX/2026", "542")
-        );
+                new CreditCardResponseDto("", "69919462069", "1000", "5465 1946 1186 2989", "04/XX/2026", "542"));
 
         return listMock.stream().filter(dto -> dto.numero().equals(numberCreditCard)).findFirst()
-                .orElse( new CreditCardResponseDto("", "69919462063", "1000", "5465 1946 1186 2986", "01/2026", "542"));
+                .orElse(new CreditCardResponseDto("", "69919462063", "1000", "5465 1946 1186 2986", "01/2026", "542"));
 
-//        ResponseEntity<CreditCardResponseDto> response = restTemplate.getForEntity(
-//                String.format("%s/{numero}", urlGetCreditCard),
-//                CreditCardResponseDto.class,
-//                numberCreditCard
-//        );
-//
-//        if (response.getStatusCode() == HttpStatus.NOT_FOUND) {
-//            throw new ValidationException("Erro ao realizar busca do cartão de credito");
-//        }
-//
-//        if (response.getBody() == null) {
-//            throw new ValidationException("Cartão de créditdo não encontrado!");
-//        }
-//
-//        return response.getBody();
+        // ResponseEntity<CreditCardResponseDto> response = restTemplate.getForEntity(
+        // String.format("%s/{numero}", urlGetCreditCard),
+        // CreditCardResponseDto.class,
+        // numberCreditCard
+        // );
+        //
+        // if (response.getStatusCode() == HttpStatus.NOT_FOUND) {
+        // throw new ValidationException("Erro ao realizar busca do cartão de credito");
+        // }
+        //
+        // if (response.getBody() == null) {
+        // throw new ValidationException("Cartão de créditdo não encontrado!");
+        // }
+        //
+        // return response.getBody();
     }
 
     private static boolean isDateBeforeCurrentDate(String cardValidateDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
 
         try {
             LocalDate cardDate = LocalDate.parse(("01/").concat(cardValidateDate), formatter);
@@ -118,6 +130,5 @@ public class ValidateCreditCard implements PaymentValidation {
             throw new ValidationException("Data no formato inválido. Use MM/yyyy.");
         }
     }
-
 
 }
--- src/test/java/com/fiap/hackathon_payment_management/adapters/validation/ValidationTest.java
+++ src/test/java/com/fiap/hackathon_payment_management/adapters/validation/ValidationTest.java
@@ -1,23 +1,28 @@
 package com.fiap.hackathon_payment_management.adapters.validation;
 
-import com.fiap.hackathon_payment_management.adapters.validation.exception.ValidationException;
-import com.fiap.hackathon_payment_management.domain.entity.Payment;
-import com.fiap.hackathon_payment_management.usecase.GetPaymentsByClient;
-import com.fiap.hackathon_payment_management.usecase.dto.PaymentDto;
-import com.fiap.hackathon_payment_management.usecase.dto.PaymentRequestDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
 
-public class ValidationTest {
+import com.fiap.hackathon_payment_management.adapters.validation.exception.ValidationException;
+import com.fiap.hackathon_payment_management.domain.entity.Payment;
+import com.fiap.hackathon_payment_management.usecase.GetPaymentsByClient;
+import com.fiap.hackathon_payment_management.usecase.dto.PaymentDto;
+import com.fiap.hackathon_payment_management.usecase.dto.PaymentRequestDto;
+
+class ValidationTest {
+
+    @Mock
+    private RestTemplate restTemplate;
 
     @Mock
     private GetPaymentsByClient getPaymentsByClient;
@@ -29,7 +34,7 @@ public class ValidationTest {
     public ValidationTest() {
         this.paymentValidations = new ArrayList<>();
         this.paymentValidations2 = new ArrayList<>();
-        this.paymentValidations.add(new ValidateCreditCard());
+        this.paymentValidations.add(new ValidateCreditCard(restTemplate, getPaymentsByClient));
         this.paymentValidations.add(new ValidatePaymentValue());
         this.paymentValidations2.add(new ValidatePaymentValue());
     }
@@ -44,7 +49,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462063x", "5465 1946 1186 2986", "01/2026", "542", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -53,7 +60,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462063", "5465 1946 1186 2986X", "01/2026", "542", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -62,7 +71,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462063", "5465 1946 1186 2986", "X01/01/2026", "542", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -71,7 +82,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462065", "5465 1946 1186 2985", "01/2020", "542", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -80,7 +93,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462069", "5465 1946 1186 2989", "04/XX/2026", "542", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -89,7 +104,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462063", "5465 1946 1186 2986", "01/01/2026", "542X", "1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 
@@ -107,7 +124,9 @@ public class ValidationTest {
 
         var requestDto = new PaymentRequestDto("69919462063", "5465 1946 1186 2986", "01/2026", "542", "-1.2");
 
-        assertThrows(ValidationException.class, () -> paymentValidations2.forEach(validator -> validator.validate(requestDto)), "Cartão de crédito não pertence a esse cliente.");
+        assertThrows(ValidationException.class,
+                () -> paymentValidations2.forEach(validator -> validator.validate(requestDto)),
+                "Cartão de crédito não pertence a esse cliente.");
 
     }
 }
