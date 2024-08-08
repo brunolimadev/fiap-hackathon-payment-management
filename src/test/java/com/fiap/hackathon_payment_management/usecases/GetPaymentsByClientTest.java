e3084bb74e97d550bf34265f9003813b6c5d7ebe
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-08 20:52:28 -0300

    chore: remove validation

--- src/test/java/com/fiap/hackathon_payment_management/usecases/GetPaymentsByClientTest.java
+++ src/test/java/com/fiap/hackathon_payment_management/usecases/GetPaymentsByClientTest.java
@@ -1,23 +1,21 @@
 package com.fiap.hackathon_payment_management.usecases;
 
-import com.fiap.hackathon_payment_management.adapters.validation.exception.ValidationException;
-import com.fiap.hackathon_payment_management.domain.entity.Payment;
-import com.fiap.hackathon_payment_management.domain.repository.PaymentRepository;
-import com.fiap.hackathon_payment_management.usecase.GetPaymentsByClient;
-import com.fiap.hackathon_payment_management.usecase.dto.PaymentRequestDto;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import com.fiap.hackathon_payment_management.domain.entity.Payment;
+import com.fiap.hackathon_payment_management.domain.repository.PaymentRepository;
+import com.fiap.hackathon_payment_management.usecase.GetPaymentsByClient;
+import com.fiap.hackathon_payment_management.usecase.dto.PaymentRequestDto;
 
 class GetPaymentsByClientTest {
 
@@ -45,41 +43,26 @@ class GetPaymentsByClientTest {
   @Test
   void shouldGetPaymentsByClientWithSuccess() {
 
-    //Arrange
+    // Arrange
     var paymentRequestDto = new PaymentRequestDto("100", "decricao", "metodo", "status", 100.00);
     var paymentEntityList = List.of(Payment.builder()
-            .id("1")
-            .clientKey("11111111111")
-            .status("status")
-            .paymentMethod("metodo")
-            .description("descricao")
-            .value(100.00)
-            .build());
+        .id("1")
+        .clientKey("11111111111")
+        .status("status")
+        .paymentMethod("metodo")
+        .description("descricao")
+        .value(100.00)
+        .build());
 
     when(paymentRepository.findByClientKey(anyString())).thenReturn(paymentEntityList);
 
-    //Act
+    // Act
     var response = getPaymentsByClient.execute("11111111111");
 
-    //Assert
+    // Assert
     assertThat(response)
-            .isNotEmpty()
-            .hasSize(1);
-
-  }
-
-  @Test
-  void shouldGetPaymentsByClientWithError() {
-
-    //Arrange
-    var paymentEntityList = new ArrayList<Payment>();
-
-    when(paymentRepository.findByClientKey(anyString())).thenReturn(paymentEntityList);
-
-    //Act & Assert
-    assertThatThrownBy(() -> getPaymentsByClient.execute("22222222222"))
-            .isInstanceOf(ValidationException.class)
-            .hasMessage("Nao existe pagamentos para o cliente");
+        .isNotEmpty()
+        .hasSize(1);
 
   }
 
